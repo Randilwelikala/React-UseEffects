b2c034604755267a37c3fafc44a136edee9eb02d
randilwelikala <randilgimantha646@gmail.com>
2025-03-31 16:15:44 +0530

    add useEffect function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState } from 'react';
 
 function App() {
+
+  const[count,setCount]= useState(0);
+
+  useEffect(()=>{
+
+    setTimeout(()=>{
+      setCount(count + 1);
+
+
+    },1000)
+    
+    
+  });
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          UseEffect Hook
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Count Value = {count}</p>
+        
+        
       </header>
     </div>
   );
